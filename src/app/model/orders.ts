02a0d35cca5113192e4e1ea03688abff5c44c857
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-01 13:54:37 +0200

    create mode for user and order (some properties missing)

---
+++ src/app/model/orders.ts
@@ -0,0 +1,9 @@
+// Here we define the type of data that we have
+// the type of data we get from the backend
+
+export interface Orders{
+    id: number;
+    // 
+    // name: string; // tipo user
+    // address: string; // tipo food
+}
---
+++ src/app/model/user.ts
@@ -0,0 +1,24 @@
+// Here we define the type of data that we have
+// the type of data we get from the backend
+
+export interface User{
+    id: number;
+    name: string;
+    address: string;
+    email: string;
+    phone: string;
+    password: string;
+    role: string;
+    // order
+}
+
+// private Long id;
+// private String name;
+// private String address;
+// private String email;
+// private String phone;
+// private String password;
+// private String role = "ROLE_USER";
+
+// @OneToMany(mappedBy="user")
+// private Set<Orders> order;
