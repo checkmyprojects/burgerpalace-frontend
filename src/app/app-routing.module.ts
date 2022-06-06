4d98d66dcd81aa2bfca281a54a16f62bbc392ab6
Iris <iris.vilaseca1998@gmail.com>
2022-06-06 15:20:40 +0200

    form not responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FoodComponent } from './food/food.component';
-
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
@@ -12,6 +12,10 @@ const routes: Routes = [
   {
     path: '/burgers',
     component: FoodComponent
+  },
+  {
+    path: '/signup',
+    component: SignupComponent
   }
 ];
 @NgModule({
