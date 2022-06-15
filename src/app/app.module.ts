c53f10380d165b6ae7323d883b63bffe97ef01dc
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-15 09:11:06 +0200

    login functionality test pending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { SignupformComponent } from './signupform/signupform.component';
 import { LoginComponent } from './login/login.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { CartComponent } from './cart/cart.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
