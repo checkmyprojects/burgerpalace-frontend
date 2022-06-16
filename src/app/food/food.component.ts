7fedd978709244a3d8d9c0ad80e6d064a8dd997e
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-16 16:08:03 +0200

    testing new token data from api on foodcomponent

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../model/food';
 import { FoodService } from '../service/food.service';
+import { TokenStorageService } from '../service/token-storage.service';
 
 @Component({
   selector: 'app-food',
@@ -11,11 +12,14 @@ import { FoodService } from '../service/food.service';
 export class FoodComponent implements OnInit {
 
   public foods: Food[] = [];
-
-  constructor( private foodService: FoodService) { }
+  currentUser: any;
+  constructor( private foodService: FoodService, private token: TokenStorageService) { }
 
   ngOnInit(): void {
     this.getFoods();
+    this.currentUser = this.token.getUser();
+    console.log("Current user is:" + this.currentUser);
+    console.log(this.currentUser);
   }
 
   public getFoods(): void{
