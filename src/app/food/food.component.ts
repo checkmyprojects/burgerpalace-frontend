45a64fe7e3c46a47225f3e25baca238c1feeff6e
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-19 11:57:33 +0200

    added bugers category. Needs to remove stuff at the bottom

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,8 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+//import { ActivatedRoute } from '@angular/router';
 import { Food } from '../model/food';
+import { CartService } from '../service/cart.service';
 import { FoodService } from '../service/food.service';
 import { TokenStorageService } from '../service/token-storage.service';
+import { ActivatedRoute,Router } from '@angular/router';
 
 @Component({
   selector: 'app-food',
@@ -13,7 +16,7 @@ export class FoodComponent implements OnInit {
 
   public foods: Food[] = [];
   currentUser: any;
-  constructor( private foodService: FoodService, private token: TokenStorageService) { }
+  constructor( private activatedRoute: ActivatedRoute,private foodService: FoodService, private cartService:CartService,private router:Router, private token: TokenStorageService) { }
 
   ngOnInit(): void {
     this.getFoods();
@@ -23,7 +26,7 @@ export class FoodComponent implements OnInit {
   }
 
   public getFoods(): void{
-    this.foodService.getAllFood().subscribe({
+    this.foodService.getFoodByCategoryName('burger').subscribe({
       next: (response: Food[]) => {
         this.foods = response;
         console.log(this.foods);
@@ -32,6 +35,10 @@ export class FoodComponent implements OnInit {
         alert(error.message);
       }
     })
+  }
+  public addToCart(food:Food):void{
+    this.cartService.addToCart(food);
+    //this.router.navigateByUrl('/cart')
   }
 
 }
