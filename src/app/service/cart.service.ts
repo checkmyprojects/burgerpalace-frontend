7b272d22ab68260fa28a658f0df11a1eb249d3a9
Iris <iris.vilaseca1998@gmail.com>
2022-06-20 11:26:48 +0200

    removeCartItem working

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Cart } from '../model/cart';
 import { CartItem } from '../model/cartItem';
@@ -9,6 +10,7 @@ import { Food } from '../model/food';
   export class CartService {
     public cart:Cart = new Cart();
     public addToCart(food:Food):void{
+        this.localStorageGetCart();
         let cartItem= this.cart.items.find(item=>item.food.id===food.id)
         
         if(cartItem){
@@ -19,10 +21,12 @@ import { Food } from '../model/food';
         this.localStorageSaveCart();
         
     }
-    removeFromCart(foodId:Number):void{
+    public removeFromCart(foodId:Number):void{
+        this.localStorageGetCart();
         this.cart.items=this.cart.items.filter(item=>
             item.food.id !=foodId);
             this.localStorageSaveCart();
+            window.location.reload();
     }
     changeQuantity(foodId:number, quantity:number){
         let cartItem=this.cart.items.find(item=>item.food.id===foodId);
